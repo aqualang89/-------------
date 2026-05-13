567594938c3b5c11b7f803de088ef5798e941004
aqualang89 <aqualang89@github.com>
2026-05-13 12:14:18 +0200

    fix(analytics): Yandex.Metrika via onMounted for client-only init

--- app.vue
+++ app.vue
@@ -1,17 +1,41 @@
 <script setup>
 useHead({
-  script: [
-    {
-      innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=109183656','ym');ym(109183656,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",accurateTrackBounce:true,trackLinks:true});`,
-      type: 'text/javascript',
-      tagPosition: 'bodyClose'
-    }
-  ],
   noscript: [
     {
       innerHTML: '<div><img src="https://mc.yandex.ru/watch/109183656" style="position:absolute; left:-9999px;" alt="" /></div>'
     }
   ]
+})
+
+onMounted(() => {
+  if (typeof window === 'undefined') return
+
+  const w = window
+  const d = document
+  const s = 'script'
+  const src = 'https://mc.yandex.ru/metrika/tag.js?id=109183656'
+
+  w.ym = w.ym || function () { (w.ym.a = w.ym.a || []).push(arguments) }
+  w.ym.l = new Date().getTime()
+
+  for (let j = 0; j < d.scripts.length; j++) {
+    if (d.scripts[j].src === src) return
+  }
+
+  const k = d.createElement(s)
+  const a = d.getElementsByTagName(s)[0]
+  k.async = 1
+  k.src = src
+  a.parentNode.insertBefore(k, a)
+
+  w.ym(109183656, 'init', {
+    ssr: true,
+    webvisor: true,
+    clickmap: true,
+    ecommerce: 'dataLayer',
+    accurateTrackBounce: true,
+    trackLinks: true
+  })
 })
 </script>
 
